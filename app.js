1d4646e7f60649fd90ed6cc710e7d09743da6c67
Edvins Antonovs <ummahusla@gmail.com>
2016-07-21 21:44:46 +0100

    Added basic req & res methods

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ var express = require('express')
 , http = require('http')
 , server = http.createServer(app)
 , io = require('socket.io').listen(server);
+
+// Map statis request to the /public folder
+app.use(express.static(__dirname + '/public'));
+
+// Request for the root to the index.html
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/public/index.html');
+});
+
+// Redirect the user to default.html if URL doesnt include filename
+app.get('/', function(request, response) {
+    response.redirect('default.html');
+});
