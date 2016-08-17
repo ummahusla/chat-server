1baf15091d3a813b3008241bf2483de3e48ab09f
Edvins Antonovs <ummahusla@gmail.com>
2016-08-17 20:01:34 +0100

    :+1: Proudly added Reactjs

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -91,7 +91,7 @@ var TopBarBox = React.createClass({
 var ChatNoticeBox = React.createClass({
     render: function() {
         return (
-            <p><span id="#greeting">Bonsua!</span> <a href="https://github.com/ummahusla/Chat-Server/">Chat Server</a> is built with <a href="https://nodejs.org">Node.js</a>, <a href="http://expressjs.com/">Express.js</a> and <a href="http://socket.io/">Socket.io</a></p>
+            <p><span id="#greeting">Bonsua!</span> <a href="https://github.com/ummahusla/Chat-Server/">Chat Server</a> is built with <a href="https://facebook.github.io/react/">React</a>, <a href="https://nodejs.org">Node.js</a>, <a href="http://expressjs.com/">Express.js</a> and <a href="http://socket.io/">Socket.io</a></p>
         )
     }
 });
